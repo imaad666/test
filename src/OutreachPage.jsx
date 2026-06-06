e523dee74dfcd37ed83dfabba8a883ba156a17e3
imaad <76239158+imaad666@users.noreply.github.com>
2026-06-07 04:30:09 +0530

    Open IDE: update src/OutreachPage.jsx

---
+++ src/OutreachPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const OutreachPage = () => {
+  return (
+    <div>
+      <h1>Outreach Page</h1>
+      <p>This is the outreach page.</p>
+    </div>
+  );
+};
+
+export default OutreachPage;
