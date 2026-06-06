dd4f26c9e59c5993632fc9145d7293d77bc31cc8
imaad <76239158+imaad666@users.noreply.github.com>
2026-06-07 04:49:46 +0530

    Open IDE: update src/helpers/routes.js

--- src/helpers/routes.js
+++ src/helpers/routes.js
@@ -1,4 +1,4 @@
-export const routes = [
-  { path: '/', component: 'App' },
-  { path: '/outreach', component: 'OutreachPage' },
-];
+export default {
+  NEW_PAGE: '/new',
+  OUTREACH_PAGE: '/outreach',
+};
