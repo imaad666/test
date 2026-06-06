9fbb0b895e5587a123043d51521ba7b2a64bc3b8
imaad <76239158+imaad666@users.noreply.github.com>
2026-06-07 01:33:52 +0530

    Open IDE: update server.js

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 import express from 'express';
-import { testController } from './controllers/testController.js';
+import { validate } from './helper.js';
 
 const app = express();
-const PORT = 3000;
-
 app.use(express.json());
 
-app.get('/test', testController);
+const PORT = 3000;
+
+// A simple route
+app.get('/api/ping', (req, res) => {
+  res.status(200).send('Pong');
+});
+
+// A route with validation example
+app.post('/api/echo', (req, res) => {
+  const { error, value } = validate(req.body);
+  if (error) {
+    res.status(400).send({ message: error });
+  } else {
+    res.send(value);
+  }
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
