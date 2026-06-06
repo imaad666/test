9c94ec5d6538a31041a5ac8f6b353e994c140953
imaad <76239158+imaad666@users.noreply.github.com>
2026-06-07 04:49:45 +0530

    Open IDE: update src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NewPage from './NewPage';
 import OutreachPage from './OutreachPage';
+import routes from './helpers/routes';
+import api from './helpers/api';
 
-const App = () => {
+function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/outreach" element={<OutreachPage />} />
-        {/* Add other routes here */}
+        <Route path={routes.NEW_PAGE} element={<NewPage />} />
+        <Route path={routes.OUTREACH_PAGE} element={<OutreachPage />} />
       </Routes>
     </BrowserRouter>
   );
-};
+}
 
 export default App;
