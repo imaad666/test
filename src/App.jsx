a4ec9bdfc49aad21d610341c8e054b42df679470
imaad <76239158+imaad666@users.noreply.github.com>
2026-06-07 01:33:53 +0530

    Open IDE: update src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { getTestData } from './api';
+import React from 'react';
 
 function App() {
-  const [testData, setTestData] = useState(null);
-
-  useEffect(() => {
-    const fetchTestData = async () => {
-      const data = await getTestData();
-      setTestData(data);
-    };
-    fetchTestData();
-  }, []);
-
-  return (
-    <div>
-      <h1>Test Data</h1>
-      {testData ? <p>{testData}</p> : <p>Loading...</p>}
-    </div>
-  );
+  return <div>Hello, World!</div>;
 }
 
 export default App;
